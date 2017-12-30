169045760528bf1945db51658b853ed1db6b04b6
Thomas Schatt <thomas.schatt@bluewin.ch>
2017-12-30 22:43:20 +0100

    Feat: Add Authentication

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
+    User        = require("./models/user"),
     seedDB      = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp", 
-    {useMongoClient: true});
+    {useMongoClient: true}); //add error handling if no connection is possible!
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again I am the best in the world!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//call this function on every single ROUTE
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get('/', function (req, res) {
     res.render("landing");
@@ -38,7 +59,7 @@ app.post("/campgrounds", function (req, res) {
     //Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
-            console.log(err);
+             console.log(err);
         } else {
             //redirect back to campgrounds page
             res.redirect("/campgrounds");
@@ -66,7 +87,7 @@ app.get("/campgrounds/:id", function(req, res){
 // ==================================
 // COMMENTS ROUTES
 // ==================================
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     //find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if (err) {
@@ -77,7 +98,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });    
 });
 
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function (req, res) {
     //lookup campground using ID
     Campground.findById(req.params.id, function(err, campground){
         if (err) {
@@ -98,9 +119,58 @@ app.post("/campgrounds/:id/comments", function (req, res) {
             });
         }
     });
-    
-    
 });
+
+// ===========
+// AUTH ROUTES
+// ===========
+
+//show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+//handle sign up logic
+app.post("/register", function(req, res){
+    //res.send("You are registered!"); //only for testing
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        } 
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+//show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//hnadle login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+    //res.send("login logic happens here!");
+});
+
+// logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, "0.0.0.0", function () {
     console.log('Server is listening on port 3000!!');
